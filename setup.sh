c0144f65d1304e1ec1c10e3ecd27d1e1e3acc53d
Danno <dannymiddlefon@gmail.com>
2020-04-14 19:59:09 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ anydesk_finished() {
 	echo $password
 	echo $(anydesk --get-id)
 }
+sudo apt -y install curl
 sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk && anydesk_finished
+sudo apt -y update
+sudo apt -y install anydesk && anydesk_finished
